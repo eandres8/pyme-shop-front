81789f18077e2d8f83459333679f5b14f8034a6a
Andres Vega <elb.andres8@gmail.com>
2026-03-24 00:41:56 -0500

    feat: add basic envs

--- src/config/http-client.ts
+++ src/config/http-client.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const apiBaseUrl = import.meta.env.VITE_API_URL;
+
 export const httpClient = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: apiBaseUrl,
 });
